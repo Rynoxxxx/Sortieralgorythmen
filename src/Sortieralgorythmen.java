a4ddcf92f486d97f118283b14844a5b8860e2f78
samuel.stroot <samuel.stroot@gmail.com>
2024-02-01 09:51:40 +0100

    manno

---
+++ src/Sortieralgorythmen.java
@@ -0,0 +1,31 @@
+
+public class Sortieralgorythmen {
+    List <Integer> insertionU;
+    List <Integer> insertionS;
+    List <Integer> selectionU;
+    List <Integer> selectionS;
+    int x=0;
+    public Sortieralgorythmen() {
+        insertionU=new List<>();
+        insertionS=new List<>();
+        selectionU=new List<>();
+        selectionS=new List<>();
+
+        while(x>10){
+            insertionU.append((int) (Math.random()*10));
+            selectionU.append((int) (Math.random()*10));
+            x++;
+        }
+        insertionU.toFirst();
+        selectionU.toFirst();
+        while(insertionU.hasAccess()) {
+            insertionS.append(insertionU.getContent());
+            insertionU.next();
+            if(insertionU.getContent()> insertionS.getContent()){
+                insertionS.append(insertionU.getContent());
+            }
+            else insertionS.insert(insertionU.getContent());
+          }
+
+    }
+}
